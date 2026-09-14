c2b90149b0533724892c67e26f7ff096bc3c9f7a
Kishan Mishra <kishanmldev@gmail.com>
2026-09-14 23:02:06 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    str_sep = "#"
+    chr_sep = "*"
+
+
+    def encode(self, strs: List[str]) -> str:
+        
+        result = self.str_sep.join(self.chr_sep.join(str(ord(ch)) for ch in st) for st in strs)
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        if not len(s):
+            return [""]
+        ans = []
+        for enc_st in s.split(self.str_sep):
+            if not enc_st:
+                ans.append("")
+            else:
+                ans.append(
+                    "".join(chr(int(ele)) for ele in enc_st.split(self.chr_sep))
+                )
+        return ans
